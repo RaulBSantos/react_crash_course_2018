5f2c4af5f8ccca053bb6a3344ab573c3721d0af0
Raul Santos <raulbolsantos@gmail.com>
2018-08-18 21:50:34 -0300

    Rendering lists, classes, and changing styles

---
+++ src/components/counter.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    imageUrl: "https://picsum.photos/200",
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
+  styles = {
+    fontSize: 50,
+    fontWeigth: "bold"
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        {/*<img src={this.state.imageUrl} alt="" />*/}
+        <span style={this.styles} className={this.getBadgeClasses()}>
+          {/* <span style={{ fontSize: 30 }} className="badge badge-primary m-2"> */}
+          {this.formatCount()}{" "}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
