695224266c2ac01d824c5361106858b9134bc7ff
Raul Santos <raulbolsantos@gmail.com>
2018-08-19 16:19:54 -0300

    Adds event binding

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,6 +7,21 @@ class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"]
   };
 
+  // Dessa forma, ou arrow functions
+  // constructor(){
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+  // handleIncrement(){
+  //   console.log('Increment Clicked', this);
+  // }
+  handleIncrement = () => {
+    // Updating this way, React don't updates the view
+    //this.state.count++; // Don't updating the view
+    // The rigth way is:
+    this.setState({ count:  this.state.count + 1 });
+  }
+
   styles = {
     fontSize: 50,
     fontWeigth: "bold"
@@ -20,7 +35,7 @@ class Counter extends Component {
           {/* <span style={{ fontSize: 30 }} className="badge badge-primary m-2"> */}
           {this.formatCount()}{" "}
         </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
         <ul>
           {this.state.tags.map(tag => (
             <li key={tag}>{tag}</li>
