57acc77c46972a82d7754481f43588f5f266d777
Raul Santos <raulbolsantos@gmail.com>
2018-08-20 23:19:43 -0300

    Passing arguments with arrow functions

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -15,32 +15,21 @@ class Counter extends Component {
   // handleIncrement(){
   //   console.log('Increment Clicked', this);
   // }
-  handleIncrement = () => {
+  handleIncrement = (param) => {
+    console.log(param);
     // Updating this way, React don't updates the view
     //this.state.count++; // Don't updating the view
     // The rigth way is:
-    this.setState({ count:  this.state.count + 1 });
+    this.setState({ count:  this.state.count + JSON.parse(param.step) });
   }
-
-  styles = {
-    fontSize: 50,
-    fontWeigth: "bold"
-  };
 
   render() {
     return (
       <React.Fragment>
-        {/*<img src={this.state.imageUrl} alt="" />*/}
-        <span style={this.styles} className={this.getBadgeClasses()}>
-          {/* <span style={{ fontSize: 30 }} className="badge badge-primary m-2"> */}
-          {this.formatCount()}{" "}
+        <span className={this.getBadgeClasses()}>
+          {this.formatCount()}
         </span>
-        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        <button onClick={() => this.handleIncrement({step: 1})} className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
